a52e05161a1865d9b024b86b1a23a0f96367625e
vandalise <95403169+vandalise@users.noreply.github.com>
2024-07-17 02:56:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,13 @@
 import random, os, ctypes, httpx, threading, time
 from colorama import init, Fore
+import sys
 
-
-os.system("cls||clear")
 init()
+os.system("cls||clear")
 
-proxies, valid_proxies = [], []
-proxies_lock = threading.Lock()
-start_time = time.time()
+checked, available, proxies, valid_proxies, proxies_lock, start_time = 0, 0, [], [], threading.Lock(), time.time()
 
-checked, available = 0, 0
-
-usernames = list(open("usernames.txt", "r").read().splitlines())
+usernames = list(open("usernames.txt", "r").read().splitlines()) if (os.path.isfile("usernames.txt") and os.path.getsize("usernames.txt") > 0) else (print("Put your usernames in usernames.txt"), input("Press Enter to exit..."), sys.exit())
 
 def worker(usernames, thread_id):
     global check
@@ -35,11 +31,8 @@ def seek(username):
             continue
         proxy = random.choice(valid_proxies)
         headers = {"accept": "*/*","content-type": "application/json","user-agent": "noplace/1 CFNetwork/1402.0.8 Darwin/22.2.0","accept-language": "en-US,en;q=0.9","accept-encoding": "gzip, deflate, br"}
-        params = {
-            "username": f"{username}"
-        }
         try:
-            response = httpx.get("https://api.nospace.app/api/v1/profiles/username/availability", headers=headers, params=params, proxies=proxy)
+            response = httpx.get("https://api.nospace.app/api/v1/profiles/username/availability", headers=headers, params={"username": f"{username}"}, proxies=proxy)
             if response.status_code == 200 or response.status_code == 204:
                 print(f"{Fore.WHITE}[{Fore.GREEN}+{Fore.WHITE}] {username}")
                 available+=1
@@ -49,7 +42,6 @@ def seek(username):
             else:
                 print(f"{Fore.WHITE}[{Fore.RED}+{Fore.WHITE}] {username}")
         except:
-            # Proxy issue
             continue
         checked+=1
         break
@@ -77,9 +69,6 @@ def check_proxy(proxy):
 
 def updateTitle():
     global checked, available
-    while True:
-        ctypes.windll.kernel32.SetConsoleTitleW(f"Usernames Checked: {checked} - Available: {available} | Proxies Scraped: {len(proxies)} - Proxies Valid: {len(valid_proxies)}")
+    while True: ctypes.windll.kernel32.SetConsoleTitleW(f"Usernames Checked: {checked} - Available: {available} | Proxies Scraped: {len(proxies)} - Proxies Valid: {len(valid_proxies)}")
 
-threading.Thread(target=updateTitle).start()
-threading.Thread(target=runner).start()
-threading.Thread(target=get_proxies).start()
+threading.Thread(target=updateTitle).start(); threading.Thread(target=runner).start(); threading.Thread(target=get_proxies).start()
